839b2ad29418f8e54c4b8352fafc99cd7331bd44
dagnall <dagnall53@googlemail.com>
2019-03-03 18:51:29 +0000

    V 8

--- Secrets.h
+++ Secrets.h
@@ -1,5 +1,5 @@
-    #define SSID_RR "SHED"         // set if you do not want to use the Serial setting system
-       #define PASS_RR "msxkhxwa"    // set if you do not want to use the Serial setting system
+    #define SSID_RR "ssid"         // set if you do not want to use the Serial setting system
+       #define PASS_RR "password"    // set if you do not want to use the Serial setting system
  //      #define myBrokerSubip          // if set will force broker to the default set below always.
                                         // this can be counterproductive if your broker address is not fixed in the Wifi DHCP settings      
        int BrokerAddrDefault = 11;   // set default broker address
